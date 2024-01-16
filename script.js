84a64d5a2fb6d6714d9e90e852558a21421c215c
waize <waizeshaikh33@gmail.com>
2024-01-16 16:23:41 +0500

    Dom

--- script.js
+++ script.js
@@ -1,32 +1,59 @@
 // ######## Add-freind Function
-const Add_Friend = function(){
-    let h1 = document.querySelector("h1")
-    let add = document.querySelector("button")
-    // let remove = document.querySelector(".remove-frr")
+// const Add_Friend = function(){
+//     let h1 = document.querySelector("h1")
+//     let add = document.querySelector("button")
+//     // let remove = document.querySelector(".remove-frr")
 
-    let a = 0
-    add.addEventListener("click", function () {
+//     let a = 0
+//     add.addEventListener("click", function () {
 
-        if (a == 0){
-            h1.innerHTML = "Frr";
-            h1.style.color = "#345"
-            add.innerHTML = "remove"
-            a = 1
-        }
+//         if (a == 0){
+//             h1.innerHTML = "Frr";
+//             h1.style.color = "#345"
+//             add.innerHTML = "remove"
+//             a = 1
+//         }
             
-        else{
-            h1.innerHTML = "starnger";
-            h1.style.color = "#000"
-            add.innerHTML = "add"
-            a = 0
-        }
+//         else{
+//             h1.innerHTML = "starnger";
+//             h1.style.color = "#000"
+//             add.innerHTML = "add"
+//             a = 0
+//         }
 
-    })
+//     })
 
-    // remove.addEventListener("click", function () {
-    //     h1.innerHTML = "remove";
-    //     h1.style.color = "#45665443"
-    // })
-}
-Add_Friend()
+//     // remove.addEventListener("click", function () {
+//     //     h1.innerHTML = "remove";
+//     //     h1.style.color = "#45665443"
+//     // })
+// }
+// Add_Friend()
 
+
+// ############ insta like Function
+
+// let  cont = document.querySelector(".cont")
+// let heart  = document.querySelector("i")
+
+// cont.addEventListener("dblclick", function(){
+//     setTimeout(function(){
+//         heart.style.opacity = 0.8
+//     },0.1000)
+
+//     setTimeout(function(){
+//         heart.style.opacity = 0
+//     },1000)
+    
+// })
+
+//  ################ Cursor move Function
+
+// let main = document.querySelector(".main");
+// let crs = document.querySelector(".cursor");
+
+// main.addEventListener("mousemove", function(dets){
+//     crs.style.left = dets.x+"px"
+//     crs.style.top = dets.y+"px" 
+    
+// })
